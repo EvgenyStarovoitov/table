599bb69f22ad199ad9e416a54cd8d9bb115f6029
Игорь Старовойтов <haner@192.168.100.5>
2018-01-09 00:24:05 +0300

    init

---
+++ webpack.config.babel.js
@@ -0,0 +1,19 @@
+import path from "path";
+module.exports = {
+  entry: ["./src/app.js"],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/[name].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+};
