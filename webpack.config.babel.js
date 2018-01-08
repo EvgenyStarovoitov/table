893661aa305dec93ed6317624caaea3fdcfbe487
Игорь Старовойтов <haner@192.168.100.5>
2018-01-09 00:43:56 +0300

    webpack config

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -3,17 +3,30 @@ module.exports = {
   entry: ["./src/app.js"],
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "js/[name].js"
+    filename: "js/[name].bundle.js"
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
+        loaders: ['babel-loader', 'eslint-loader']
+        // use: {
+        //   loader: "babel-loader"
+        // }
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'react-hot-loader!babel-loader'
       }
     ]
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "./dist/assets/media"),
+    compress: true,
+    port: 12000,
+    stats: 'errors-only',
+    open: true
   }
 };
